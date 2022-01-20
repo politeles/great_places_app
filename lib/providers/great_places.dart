8795b9e1edff6ffe094d8eae5aa65b1923cfaa45
José Enrique <politeles@users.noreply.github.com>
2022-01-20 18:32:08 +0100

    sqlDB support and insert implemented

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
@@ -20,5 +21,10 @@ class GreatPlaces with ChangeNotifier {
         location: null);
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image,
+    });
   }
 }
